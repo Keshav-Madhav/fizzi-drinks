7827cbfb308da28e36d709c9f3012d1428c217b0
Keshav Madhav <keshav2552003@gmail.com>
2024-09-20 14:09:47 +0530

    Initial commit with prismic, code clean up and content for Landing page

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { FizziLogo } from "@/components/FizziLogo"
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className="flex justify-center py-4 -mb-28">
+      <FizziLogo className="h-20 z-10 cursor-pointer text-sky-800 "/>
+    </header>
+  )
+}
+export default Header
