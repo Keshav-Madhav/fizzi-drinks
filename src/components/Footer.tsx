2de85420ee1a91573540b784216e9a536d563576
Keshav Madhav <keshav2552003@gmail.com>
2024-09-23 00:04:36 +0530

    Minor footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = (props: Props) => {
     <footer className="bg-[#FEE832] text-[#FE6334]">
       <div className="relative mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
         <FizziLogo />
-        <div className="absolute right-24 top-0 size-28 origin-center -translate-y-14 md:size-8/12 md:-translate-y-28">
+        <div className="absolute right-24 top-0 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
           <CircleText />
         </div>
       </div>
